11e18a7b3845319b20b86f9de9683e3ff0958bc2
fangweiyu <fangweiyu@paisongtech.com>
2021-10-17 21:35:16 +0800

    init shell and readme, update dir name
    init shell and readme, update dir name

---
+++ module3/README.md
@@ -0,0 +1,5 @@
+# module 3 homework
+
+The dockerfile build the image from [module2 homework](../module2)
+
+The `enter_container.sh` is used to enter the container, the parameter is the name of container.
---
+++ module3/enter_container.sh
@@ -0,0 +1,9 @@
+# Check input
+docker ps |grep -w $1
+if [ $? -ne 0 ];then
+echo "Container $1 not found."
+exit 1
+fi
+# Enter container
+PID=`docker inspect --format "{{ .State.Pid }}" $1`
+nsenter --target ${PID} --mount --uts --ipc --net --pid
